ec336b3454a390601dbf93de55ce4db0c66eeb4f
Ladydee3 <delisiletwala62@gmail.com>
2024-02-28 13:08:27 -0800

    changes

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preoder - travel through binart tree using pre-order traversal
+ * binary_tree_preorder - travel through binart tree using pre-order traversal
  * @tree: pointer to a root node of the tree to traverse
  * @func: function pointer for each node
  */
@@ -10,7 +10,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func) (int))
 	if (tree && func)
 	{
 	func(tree->n);
-	binary_tree_preoder(tree->left, func);
-	binary_tree_preoder(tree->right, func);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 	}
 }
